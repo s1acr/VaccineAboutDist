3b14ecc5d0807afcf288269083a352a405abcfeb
1714480752@qq.com <92618886+poisunk@users.noreply.github.com>
2023-04-30 17:20:52 +0800

    💥 重构疫苗查询页面

--- js/vaccineCFDA.js
+++ js/vaccineCFDA.js
@@ -1,88 +1,132 @@
-/*
- * @Author: Slacr 11812766+Fslacr@user.noreply.gitee.com
- * @Date: 2023-04-15 15:43:39
- * @LastEditors: Slacr 11812766+Fslacr@user.noreply.gitee.com
- * @LastEditTime: 2023-04-15 18:39:23
- * @FilePath: \VA_\js\vaccineCFDA.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
+
 $(function () {
-    $("#va_search_btn").on("click", () => {
-        let product_name = $("#product_name").val().trim()
-        if (product_name == "") return alert("输入为空")
-        init_res(1, 5, product_name)
-    })
+    initVaccineTypeList()
+    initVaccineList()
 
 
-
-    // 初始化疫苗查询结果
-    function init_res(page = 1, pageSize = 5, productName = "新冠") {
-        $("#vaccine_res").html("")
-        $.ajax({
-            type: 'GET',
-            url: `http://43.140.194.248:8080/api/vaccine/cfda?page=${page}&pageSize=${pageSize}&productName=${productName}`,
-            success: function (res) {
-                console.log(res)
-                if (res.status == 1) {
-
-                    let va_list = res.data.vaccineList
-                    // console.log(va_list)
-                    let html = handle_res_list(va_list)
-                    $("#vaccine_res").html(html)
-                }
-
-            }
-
-        })
-
-        // 点击查看详情
-        $("#vaccine_res").on("click", "#check_btn", (e) => {
-            $(e.target).prev().toggleClass("collapsed")
-
-        })
-    }
-
-    // 处理返回的疫苗数组返回HTML
-    function handle_res_list(va_list) {
-        let html = `    `
-
-        $.each(va_list, (index, value) => {
-            delete value.id
-            html += `<div id = "res_box" class = "collapsed" >`
-            $.each(value, (index, value) => {
-                if (value == "") value = "无数据"
-
-                html += `
-                    
-                    <label>${index}:&nbsp;&nbsp;</label>
-                    <p>${value}</p>
-                  
-                `
-            })
-            html += `
-                </div> 
-                <button id="check_btn" type="button" class=" btn btn-default" style="margin: 5px"></button>
-
-                <div class="divide_line"></div>   
-               
-                `
-        })
-        return html
-    }
-
-
-
-
-
-
-    //分页按钮
-    // function init_pagination() {
-    //     $("#prev").on("click", () => {
-    //         console.log($(".pagination .justify-content-center").children())
-    //     })
-    // }
-
-    init_res()
-    // init_pagination()
-
+    initSumbmitOnClickListener()
 })
+
+function initSumbmitOnClickListener() {
+    $("#vaccine-submit-button").click(function () {
+        let keyword = $("#vaccine-input-product-name").val()
+        console.log(keyword)
+        loadVaccineList(
+            onSuccess = function (json) {
+                console.log(json)
+                let vaccineList = json.data.data
+                renderVaccineList(vaccineList, true)
+            },
+            keyword = keyword
+        )
+    })
+}
+
+
+function initVaccineTypeList() {
+    loadVaccineTypeList(
+        onSuccess = function (json) {
+            let typeList = json.data.data
+            renderVaccineTypeList(typeList)
+        }
+    )
+}
+
+function initVaccineList() {
+    loadVaccineList(
+        onSuccess = function (json) {
+            let vaccineList = json.data.data
+            renderVaccineList(vaccineList)
+        }
+    )
+}
+
+function renderVaccineTypeList(typeList) {
+    $.each(typeList, function (index, e) {
+        let html = `<li>${e.type}</li>`
+        $('#vaccine-type-container').append(html)
+    })
+    $('#vaccine-type-container').on('click', 'li', function () {
+        let typeStr = $(this).text()
+        loadVaccineListByType(
+            onSuccess = function (json) {
+                console.log(json)
+                let vaccineList = json.data.data
+                renderVaccineList(vaccineList, true)
+            },
+            type = typeStr
+        )
+    })
+}
+
+function renderVaccineList(vaccineList, isClear=false) {
+    if(isClear) {
+        $('#vaccine-search-result-box').html('')
+    }
+    $.each(vaccineList, (index, e) => {
+        let html = `
+            <div class="vaccine-result-item">
+                <div class="vaccine-result-card">
+                    <div class="vaccine-result-title">${e.type}</div>
+                    <div class="vaccine-result-label">疫苗名称</div>
+                    <div class="vaccine-result-name">${e.productName}</div>
+                    <div class="vaccine-result-label">生产公司</div>
+                    <div class="vaccine-result-manu">${e.productionCompany}</div>
+                </div>
+            </div>
+        `
+        $('#vaccine-search-result-box').append(html)
+    })
+}
+
+
+function loadVaccineTypeList(onSuccess, page = 1, pageSize = 20) {
+    let query = {
+        page: page,
+        pageSize: pageSize
+    }
+    $.ajax({
+        url: "http://43.140.194.248/api/vaccine/type",
+        type: "GET",
+        dataType: "json",
+        data: query,
+        success: function (data) {
+            onSuccess(data)
+        }
+    })
+}
+
+function loadVaccineList(onSuccess, keyword = "", page = 1, pageSize = 20) {
+    let query = {
+        page: page,
+        pageSize: pageSize,
+        productName: keyword
+    }
+    console.log(query)
+    $.ajax({
+        url: "http://43.140.194.248/api/vaccine/cfda",
+        type: "GET",
+        dataType: "json",
+        data: query,
+        success: function (data) {
+            onSuccess(data)
+        }
+    })
+}
+
+function loadVaccineListByType(onSuccess, type, page = 1, pageSize = 20) {
+    let query = {
+        page: page,
+        pageSize: pageSize,
+        type: type
+    }
+    $.ajax({
+        url: "http://43.140.194.248/api/vaccine/cfda",
+        type: "GET",
+        dataType: "json",
+        data: query,
+        success: function (data) {
+            onSuccess(data)
+        }
+    })
+}
