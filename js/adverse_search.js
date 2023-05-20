f59686ae442dc6ac38c2a7716c0095e7aa57d0f8
1714480752@qq.com <92618886+poisunk@users.noreply.github.com>
2023-05-20 23:22:45 +0800

    ✨ 添加跳转到详情页面

--- js/adverse_search.js
+++ js/adverse_search.js
@@ -28,7 +28,9 @@ function searchAdverse(keyword) {
 function handleAdverse(list) {
     $.each(list, (index, e) => {
         var dom = createResultItem(e)
-        dom.click(showDetailCard(e))
+        dom.click(function () {
+            window.open(`/page/adverse_detail.html?adverseId=${e.id}`)
+        })
         $('#search-result-box').append(dom)
     })
 }
@@ -69,43 +71,3 @@ function createResultItem(result) {
     })
     return element
 }
-
-function showDetailCard(e) {
-    return () => {
-        $('.bottom-detail-card').show()
-        let names = Object.getOwnPropertyNames(e)
-        $.each(names, (i, s) => {
-            eval(`$('.bottom-detail-card').find("#detail-${s}").text(e.${s})`)
-        })
-
-        $("#detail-item-symptom").empty()
-        $("#detail-item-symptom").append("<tr><th>症状</th><th>OAE</th></tr>")
-        $.each(e.symptomList, (i, s) => {
-            let html = `
-                <tr>
-                    <td>${s.symptom}</td>
-                    <td>${s.oaeIRI}</td>
-                </tr>
-            `
-            $("#detail-item-symptom").append(html)
-        })
-
-        $("#detail-item-vaccine").empty()
-        $("#detail-item-vaccine").append("<tr><th>疫苗类型</th><th>疫苗名称</th><th>制造商</th><th>接种日期</th><th>接种剂次</th><th>接种途径</th><th>接种部位</th></tr>")
-        $.each(e.vaccineList, (i, v) => {
-            let html = `
-                <tr>
-                    <td>${v.type}</td>
-                    <td>${v.name}</td>
-                    <td>${v.manufacturer}</td>
-                    <td>${v.vaccinateDate}</td>
-                    <td>${v.dose}</td>
-                    <td>${v.route}</td>
-                    <td>${v.site}</td>
-                </tr>
-            `
-            $("#detail-item-vaccine").append(html)
-        })
-    }
-}
-
