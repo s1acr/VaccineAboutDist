280b02893973e2e556abbbc2ae46003ac0e0c343
1714480752@qq.com <92618886+poisunk@users.noreply.github.com>
2023-05-30 18:09:37 +0800

    ✨ 为不良反应报告添加跳转到疫苗已经oae词条页面的功能

--- js/adverse_detail.js
+++ js/adverse_detail.js
@@ -18,6 +18,7 @@ function initPage() {
 }
 
 function renderAdverseDetail(detail) {
+    console.log(detail)
     let names = Object.getOwnPropertyNames(detail)
     $.each(names, (i, s) => {
         eval(`$('#main-content').find("#detail-${s}").text(detail.${s})`)
@@ -26,34 +27,36 @@ function renderAdverseDetail(detail) {
     $("#detail-item-symptom").empty()
     $("#detail-item-symptom").append("<tr><th>症状</th><th>OAE</th></tr>")
     $.each(detail.symptomList, (i, s) => {
-        let html = `
+        let html = $(`
                 <tr>
                     <td>${s.symptom}</td>
-                    <td>${s.oaeIRI}</td>
+                    <td class="oae_term clickable_item">${s.oaeIRI}</td>
                 </tr>
-            `
+            `)
+        html.find("td.oae_term").on('click', () => {
+            window.open(`../oaeLabel.html?oaeId=${s.oaeId}`)
+        })
         $("#detail-item-symptom").append(html)
     })
 
     $("#detail-item-vaccine").empty()
     $("#detail-item-vaccine").append("<tr><th>疫苗类型</th><th>疫苗名称</th><th>制造商</th><th>接种日期</th><th>接种剂次</th><th>接种途径</th><th>接种部位</th></tr>")
     $.each(detail.vaccineList, (i, v) => {
-        let html = `
+        let html = $(`
                 <tr>
                     <td>${v.type}</td>
-                    <td class="vaccine_name clickable_item" id="vaccine_${1}">${v.name}</td>
+                    <td class="vaccine_name clickable_item">${v.name}</td>
                     <td>${v.manufacturer}</td>
                     <td>${v.vaccinateDate}</td>
                     <td>${v.dose}</td>
                     <td>${v.route}</td>
                     <td>${v.site}</td>
                 </tr>
-            `
+            `)
+        html.find("td.vaccine_name").on('click', () => {
+            window.open(`./vaccine_detail.html?vaccineId=${v.id}`)
+        })
         $("#detail-item-vaccine").append(html)
-    })
-    $("#detail-item-vaccine").find("td.vaccine_name").on("click", () => {
-        let vaccineId = $(this).attr("id").split("_")[1]
-        window.open(`./vaccine_detail.html?vaccineId=${vaccineId}`)
     })
 }
 
